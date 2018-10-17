cce743bc16bc22365e10874c87813da22d6b0cad
Bibek Gupta <bibek.gupta@okstate.edu>
2018-10-17 08:29:50 -0500

    Initial commit

---
+++ LessonSix.java
@@ -0,0 +1,185 @@
+import java.util.Scanner; 
+
+import java.util.*; 
+
+import java.io.*; // Allows for system input and output through data streams, serialization and the file system
+
+ 
+
+public class LessonSix {
+
+     
+
+    // Creates a Scanner object that monitors keyboard input
+
+    static Scanner userInput = new Scanner(System.in);
+     
+
+    public static void main(String[] args){
+
+         
+
+        divideByZero(2);
+
+         
+
+        System.out.print("How old are you? ");
+
+        int age = checkValidAge();
+
+         
+
+        if (age != 0){
+
+        System.out.println("You are " + age + " years old");
+
+        }
+
+         
+
+        getAFile("./somestuff.txt");
+
+         
+
+    }
+
+     
+
+    public static void divideByZero(int a)
+
+    {
+
+         
+
+        try
+
+        {
+
+            System.out.println(a/0);
+
+        }
+
+        
+        catch (ArithmeticException e)
+
+        {
+
+            // Your custom error message
+
+            System.out.println("You can't divide by zero");
+
+             
+
+            // Java's error message for this exception
+
+            System.out.println(e.getMessage());
+
+             
+
+            // Prints the exception name and error message
+
+            System.out.println(e.toString());
+
+            // Prints the standard error stack trace
+
+            e.printStackTrace();
+
+        }
+
+         
+
+    }
+
+     
+
+    public static int checkValidAge()
+
+    {
+
+         
+
+        try
+
+        {
+
+            return userInput.nextInt(); // nextInt() receives the user input
+
+        }
+
+         
+
+        catch (InputMismatchException e)
+
+        {
+
+            userInput.next(); // Skips the last user input and waits for the next
+
+            System.out.print("That isn't a whole number");
+
+            return 0;
+
+        }
+
+         
+
+    }
+
+    public static void getAFile(String fileName)
+
+    {
+
+        try
+
+        {
+
+        FileInputStream file = new FileInputStream(fileName);
+
+        }
+
+         
+
+        catch (FileNotFoundException e)
+
+        {
+
+            System.out.println("Sorry I couldn't find that file");
+
+        }
+
+         
+
+        catch (IOException e) // Catches any IO Exception
+
+        {
+
+            System.out.println("An unknown IO Error Occured");
+
+        }
+
+        //catch(ClassNotFoundException I IOException e){} 
+
+        catch (Exception e)
+
+        {
+
+            System.out.println("I catch every exception");
+
+        }
+
+         
+
+        finally
+
+        {
+
+            System.out.println("");
+
+        }
+
+         
+
+    }
+
+     
+
+}
